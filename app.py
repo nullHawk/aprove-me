312421e9b692f64fd6d4a372e72eefc7cd7a0d57
Suryansh <suryanshsinghshakya1@gmail.com>
2025-04-05 13:43:36 +0000

    add: prediction

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open("model/classifier.pkl", "rb"))
+
+@app.route("/prediction", methods=["POST"])
+def predict():
+    loan_req = request.get_json()
+    print(loan_req)
+    if loan_req['Gender'] == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+    if loan_req['Married'] == "Unmarried":
+        Married = 0
+    else:
+        Married = 1
+    if loan_req['Credit_History'] == "Unclear Debts":
+        Credit_History = 0
+    else:
+        Credit_History = 1
+
+    ApplicantIncome = loan_req['ApplicantIncome']
+    LoanAmount = loan_req['LoanAmount']
+
+    result = model.predict([[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
+
+    if result == 0:
+        pred = "Rejected"
+    else:
+        pred = "Approved"
+
+    return {"loan_approval_status": pred}
